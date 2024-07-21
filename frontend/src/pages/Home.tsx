ee0c1c185571689b8bb199333f0924d1e659a761
Eunicekk <cho_lyn@naver.com>
2024-07-21 20:48:34 +0900

    [feat] Update home - banner S11P12A806-98

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,11 +1,15 @@
 import "@css/Home.css";
 import categories from "@utils/get-category-image";
+import Banner from "@components/Home/Banner";
+import banners from "@utils/get-banner-image";
 import CategoryButton from "@components/Home/CategoryButton";
 
 const Home = () => {
   return (
     <div id="home">
-      <section id="banner-section"></section>
+      <section id="banner-section">
+        <Banner banners={banners} />
+      </section>
       <section id="category-section">
         {categories.map((category, index) => (
           <CategoryButton
@@ -14,6 +18,15 @@ const Home = () => {
             text={category.text}
           />
         ))}
+      </section>
+      <section id="best-section">
+
+      </section>
+      <section id="open-section">
+
+      </section>
+      <section id="recommend-section">
+        
       </section>
     </div>
   );

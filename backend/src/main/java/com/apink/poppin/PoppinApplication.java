2ee5cb15da69b66202d8b56e70cb2ffec0857d63
dahwan <sinphoto@naver.com>
2024-07-22 09:51:40 +0900

    [chore] Set project

---
+++ backend/src/main/java/com/apink/poppin/PoppinApplication.java
@@ -0,0 +1,13 @@
+package com.apink.poppin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoppinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoppinApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/apink/poppin/PoppinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apink.poppin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoppinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

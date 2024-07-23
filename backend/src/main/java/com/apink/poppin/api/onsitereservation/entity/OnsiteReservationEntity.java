3520d88afc273f08108324e883b03d1051ac03b7
parseler <gruns0989@naver.com>
2024-07-23 09:17:35 +0900

    [feat] Delete onsite-reservation packages

--- backend/src/main/java/com/apink/poppin/api/onsitereservation/entity/OnsiteReservationEntity.java
+++
@@ -1,4 +0,0 @@
-package com.apink.poppin.api.onsitereservation.entity;
-
-public class OnsiteReservationEntity {
-}
--- backend/src/main/java/com/apink/poppin/api/onsitereservation/service/OnsiteReservationService.java
+++
@@ -1,4 +0,0 @@
-package com.apink.poppin.api.onsitereservation.service;
-
-public interface OnsiteReservationService {
-}

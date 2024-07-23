f7109ddfd3a79461c44f6b498ecd98de082bd5ec
parseler <gruns0989@naver.com>
2024-07-23 15:55:47 +0900

    [feat] Add JSON settings to visitedDate Jira : S11P12A806-152

--- backend/src/main/java/com/apink/poppin/api/reservation/dto/OnsiteReservationDto.java
+++ backend/src/main/java/com/apink/poppin/api/reservation/dto/OnsiteReservationDto.java
@@ -1,6 +1,13 @@
 package com.apink.poppin.api.reservation.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import lombok.*;
+
+import java.time.LocalDate;
 
 @Getter
 @Setter
@@ -11,7 +18,11 @@ public class OnsiteReservationDto {
     private Long onsiteReservationId;
     private Long popupId;
     private String phoneNumber;
-//    private LocalDate visitedDate;
+
+    @JsonSerialize(using = LocalDateSerializer.class)
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private LocalDate visitedDate;
     private Integer reservationStatementId;
     private Integer reservationCount;
     private Integer waitNumber;

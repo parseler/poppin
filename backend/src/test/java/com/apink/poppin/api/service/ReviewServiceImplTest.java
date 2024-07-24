e2b31d48435f506c248659d24c087d7a93334d70
parseler <gruns0989@naver.com>
2024-07-24 13:59:53 +0900

    [feat] Add test code for review service Jira : S11P12A806-162

--- backend/src/test/java/com/apink/poppin/api/service/ReviewServiceImplTest.java
+++ backend/src/test/java/com/apink/poppin/api/service/ReviewServiceImplTest.java
@@ -68,10 +68,56 @@ public class ReviewServiceImplTest {
         long reviewId = 1L;
         when(reviewRepository.findById(reviewId)).thenReturn(Optional.empty());
 
+        // when
         NoSuchElementException exception = assertThrows(NoSuchElementException.class,
                 () -> reviewService.getReviewById(reviewId));
 
+        // then
         assertEquals("Review not found", exception.getMessage());
         verify(reviewRepository, times(1)).findById(reviewId);
+    }
+
+    @Test
+    void updateReviewSuccess() {
+        // given
+        long reviewId = 1L;
+        Popup popup = mock(Popup.class);
+        User user = mock(User.class);
+        List<Comment> comments = new ArrayList<>();
+
+        Review existingReview = Review.builder()
+                .reviewId(reviewId)
+                .popup(popup)
+                .user(user)
+                .rating(5.0F)
+                .title("existing title")
+                .thumbnail("existing thumbnail")
+                .content("existing content")
+                .createdAt(Instant.now())
+                .comments(comments)
+                .build();
+
+        ReviewDto updatedReviewDto = ReviewDto.builder()
+                .reviewId(reviewId)
+                .popupId(popup.getPopupId())
+                .userTsid(user.getUserTsid())
+                .rating(4.0F)
+                .title("updated title")
+                .thumbnail("updated thumbnail")
+                .content("updated content")
+                .build();
+
+        // when
+        when(reviewRepository.findById(reviewId)).thenReturn(Optional.ofNullable(existingReview));
+
+        // then
+        reviewService.updateReview(updatedReviewDto);
+
+        assertEquals(existingReview.getRating(), updatedReviewDto.getRating());
+        assertEquals(existingReview.getTitle(), updatedReviewDto.getTitle());
+        assertEquals(existingReview.getThumbnail(), updatedReviewDto.getThumbnail());
+        assertEquals(existingReview.getContent(), updatedReviewDto.getContent());
+        verify(reviewRepository, times(1)).findById(reviewId);
+        verify(reviewRepository, times(1)).save(existingReview);
     }
 }

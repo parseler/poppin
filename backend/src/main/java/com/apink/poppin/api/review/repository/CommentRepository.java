a3d18b63191319cb93061d153eee772b2a1bf37d
parseler <gruns0989@naver.com>
2024-07-24 16:17:24 +0900

    [feat] Add CommentRepository.java Jira : S11P12A806-162

--- backend/src/main/java/com/apink/poppin/api/review/repository/CommentRepository.java
+++ backend/src/main/java/com/apink/poppin/api/review/repository/CommentRepository.java
@@ -4,9 +4,7 @@ import com.apink.poppin.api.review.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    List<Comment> findByReviewId(long reviewId);
+
 }

a225af7511172227c52071b1221cb729d2ad945a
parseler <gruns0989@naver.com>
2024-07-25 15:25:14 +0900

    [feat] Add delete method for ReviewController.java Jira : S11P12A806-166

--- backend/src/main/java/com/apink/poppin/api/review/controller/ReviewController.java
+++ backend/src/main/java/com/apink/poppin/api/review/controller/ReviewController.java
@@ -1,7 +1,9 @@
 package com.apink.poppin.api.review.controller;
 
+import com.apink.poppin.api.review.dto.CommentDto;
 import com.apink.poppin.api.review.dto.ReviewDto;
 import com.apink.poppin.api.review.dto.ReviewUpdateRequestDto;
+import com.apink.poppin.api.review.service.CommentService;
 import com.apink.poppin.api.review.service.ReviewService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 public class ReviewController {
 
     private final ReviewService reviewService;
+    private final CommentService commentService;
 
     @GetMapping("/{reviewId}")
     public ResponseEntity<?> getReview(@PathVariable long reviewId) {
@@ -30,6 +33,24 @@ public class ReviewController {
     @DeleteMapping("/{reviewId}")
     public ResponseEntity<?> deleteReview(@PathVariable long reviewId) {
         reviewService.deleteReview(reviewId);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/{reviewId}/comments")
+    public ResponseEntity<?> createComment(@PathVariable long reviewId, @RequestBody CommentDto commentDto) {
+        commentService.createComment(reviewId, commentDto);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/{reviewId}/comments/{commentId}")
+    public ResponseEntity<?> updateComment(@PathVariable long reviewId, @PathVariable long commentId, @RequestBody CommentDto commentDto) {
+        commentService.updateComment(reviewId, commentId, commentDto);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{reviewId}/comments/{commentId}")
+    public ResponseEntity<?> deleteComment(@PathVariable long reviewId, @PathVariable long commentId) {
+        commentService.deleteComment(reviewId, commentId);
         return ResponseEntity.ok().build();
     }
 }

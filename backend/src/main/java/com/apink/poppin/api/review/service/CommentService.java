d788692d507a8b05ea182564839aac12c24c6487
parseler <gruns0989@naver.com>
2024-07-25 13:53:36 +0900

    [feat] Update comment service Jira : S11P12A806-164

--- backend/src/main/java/com/apink/poppin/api/review/service/CommentService.java
+++ backend/src/main/java/com/apink/poppin/api/review/service/CommentService.java
@@ -1,5 +1,11 @@
 package com.apink.poppin.api.review.service;
 
+import com.apink.poppin.api.review.dto.CommentDto;
+import com.apink.poppin.api.review.entity.Comment;
+
 public interface CommentService {
 
+    Comment createComment(CommentDto commentDto);
+
+    void updateComment(CommentDto commentDto);
 }
--- backend/src/main/java/com/apink/poppin/api/review/service/CommentServiceImpl.java
+++ backend/src/main/java/com/apink/poppin/api/review/service/CommentServiceImpl.java
@@ -1,6 +1,14 @@
 package com.apink.poppin.api.review.service;
 
+import com.apink.poppin.api.review.dto.CommentDto;
+import com.apink.poppin.api.review.entity.Comment;
+import com.apink.poppin.api.review.entity.Review;
 import com.apink.poppin.api.review.repository.CommentRepository;
+import com.apink.poppin.api.review.repository.ReviewRepository;
+import com.apink.poppin.api.test.entity.User;
+import com.apink.poppin.api.test.repository.UserRepository;
+import com.apink.poppin.common.exception.dto.BusinessLogicException;
+import com.apink.poppin.common.exception.dto.ExceptionCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +17,46 @@ import org.springframework.stereotype.Service;
 public class CommentServiceImpl implements CommentService {
 
     private final CommentRepository commentRepository;
+    private final ReviewRepository reviewRepository;
+    private final UserRepository userRepository;
+
+    @Override
+    public Comment createComment(CommentDto commentDto) {
+        Review review = getReview(commentDto.getReviewId());
+        User user = getUser(commentDto.getUserTsid());
+        Comment parent = getParent(commentDto.getParent());
+
+        Comment comment = new Comment();
+        comment.createComment(commentDto, review, user, parent);
+
+       return commentRepository.save(comment);
+    }
+
+    @Override
+    public void updateComment(CommentDto commentDto) {
+        Comment comment = commentRepository.findById(commentDto.getCommentId())
+                .orElseThrow(() -> new BusinessLogicException(ExceptionCode.COMMENT_NOT_FOUND));
+
+        if (comment.isDeleted()) {
+            throw new BusinessLogicException(ExceptionCode.COMMENT_ALREADY_DELETED);
+        }
+
+        comment.updateComment(commentDto.getContent());
+
+        commentRepository.save(comment);
+    }
+
+    private Review getReview(long id) {
+        return reviewRepository.findById(id)
+                .orElseThrow(() -> new BusinessLogicException(ExceptionCode.REVIEW_NOT_FOUND));
+    }
+
+    private User getUser(long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new BusinessLogicException(ExceptionCode.USER_NOT_FOUND));
+    }
+
+    private Comment getParent(long id) {
+        return commentRepository.findById(id).orElse(null);
+    }
 }

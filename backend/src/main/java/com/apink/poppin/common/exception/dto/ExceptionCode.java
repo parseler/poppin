c9ae0b57cd6129677d9fdab452e3b6dc0ab2830e
parseler <gruns0989@naver.com>
2024-07-25 13:54:42 +0900

    [feat] Add exception code for review and comment Jira : S11P12A806-164

--- backend/src/main/java/com/apink/poppin/common/exception/dto/ExceptionCode.java
+++ backend/src/main/java/com/apink/poppin/common/exception/dto/ExceptionCode.java
@@ -6,8 +6,16 @@ import lombok.Getter;
 public enum ExceptionCode {
 
     REVIEW_NOT_FOUND(404, "Review not found"),
+    REVIEW_ALREADY_DELETED(400, "Review already deleted"),
+    REVIEW_UPDATE_FAILURE(500, "Review update failure"),
+
     COMMENT_NOT_FOUND(404, "Comment not found"),
-    REVIEW_ALREADY_DELETED(400, "Review already deleted"),;
+    COMMENT_ALREADY_DELETED(400, "Comment already deleted"),
+
+    USER_NOT_FOUND(404, "User not found"),
+    ;
+
+
 
     private final int status;
     private final String message;

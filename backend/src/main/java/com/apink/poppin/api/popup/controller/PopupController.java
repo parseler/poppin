71f5acd1bb00d627e647fbaf8464edddb16ae173
min_yl8234 <min_yl8234@daum.net>
2024-07-25 15:40:15 +0900

    [refactor] Refactoring PopupController S11P12A806-138

--- backend/src/main/java/com/apink/poppin/api/popup/controller/PopupController.java
+++ backend/src/main/java/com/apink/poppin/api/popup/controller/PopupController.java
@@ -22,8 +22,8 @@ public class PopupController {
     // 팝업 전체 목록 조회 및 검색
     @GetMapping("")
     public ResponseEntity<List<PopupDTO>> getPopupList(@RequestParam(value="keyword", defaultValue="") String keyword) {
-        List<PopupDTO> list = popupService.getPopupList(keyword);
-        return new ResponseEntity<>(list, HttpStatus.OK);
+        List<PopupDTO> allList = popupService.getPopupList(keyword);
+        return new ResponseEntity<>(allList, HttpStatus.OK);
     }
 
     // 팝업 상세 조회
@@ -36,22 +36,22 @@ public class PopupController {
     // 인기 팝업 조회
 //    @GetMapping("/rank")
 //    public ResponseEntity<List<PopupDTO>> getPopupRank() {
-//        List<PopupDTO> list = popupService.getPopupRank();
-//        return new ResponseEntity<>(list, HttpStatus.OK);
+//        List<PopupDTO> rankList = popupService.getPopupRank();
+//        return new ResponseEntity<>(rankList, HttpStatus.OK);
 //    }
 
     // 유사 팝업 조회
 //    @GetMapping("/{popupId}/tag")
 //    public ResponseEntity<List<Popup>> getSimilarPopup(@PathVariable("popupId") long popupId) {
-//        List<Popup> list = popupService.getSimilarPopup(popupId);
-//        return new ResponseEntity<>(list, HttpStatus.OK);
+//        List<Popup> similarList = popupService.getSimilarPopup(popupId);
+//        return new ResponseEntity<>(similarList, HttpStatus.OK);
 //    }
 
     // 오픈 예정 팝업 조회
     @GetMapping("/open")
     public ResponseEntity<List<PopupDTO>> getOpenPopup() {
-        List<PopupDTO> list = popupService.getOpenPopup();
-        return new ResponseEntity<>(list, HttpStatus.OK);
+        List<PopupDTO> openList = popupService.getOpenPopup();
+        return new ResponseEntity<>(openList, HttpStatus.OK);
     }
 
 }

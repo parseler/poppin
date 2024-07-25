c0b872054038a9ac1f05bc9bd4a5b0bea062ada6
parseler <gruns0989@naver.com>
2024-07-25 09:55:35 +0900

    [feat] Add ExceptionCode.java Jira : S11P12A806-163

--- backend/src/main/java/com/apink/poppin/common/exception/dto/ExceptionCode.java
+++ backend/src/main/java/com/apink/poppin/common/exception/dto/ExceptionCode.java
@@ -6,7 +6,8 @@ import lombok.Getter;
 public enum ExceptionCode {
 
     REVIEW_NOT_FOUND(404, "Review not found"),
-    COMMENT_NOT_FOUND(404, "Comment not found");
+    COMMENT_NOT_FOUND(404, "Comment not found"),
+    REVIEW_ALREADY_DELETED(400, "Review already deleted"),;
 
     private final int status;
     private final String message;

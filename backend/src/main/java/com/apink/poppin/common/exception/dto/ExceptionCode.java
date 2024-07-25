82873227f42a2fb523cab4d5fa2fb1a400b5a8f6
parseler <gruns0989@naver.com>
2024-07-25 15:36:31 +0900

    [refactor] Add exception code Jira : S11P12A806-166

--- backend/src/main/java/com/apink/poppin/common/exception/dto/ExceptionCode.java
+++ backend/src/main/java/com/apink/poppin/common/exception/dto/ExceptionCode.java
@@ -7,10 +7,13 @@ public enum ExceptionCode {
 
     REVIEW_NOT_FOUND(404, "Review not found"),
     REVIEW_ALREADY_DELETED(400, "Review already deleted"),
-    REVIEW_UPDATE_FAILURE(500, "Review update failure"),
+    REVIEW_CREATE_FAILED(401, "Review creation failed"),
+    REVIEW_UPDATE_FAILED(500, "Review update failed"),
 
     COMMENT_NOT_FOUND(404, "Comment not found"),
     COMMENT_ALREADY_DELETED(400, "Comment already deleted"),
+    COMMENT_CREATE_FAILED(400, "Comment creation failed"),
+    COMMENT_UPDATE_FAILED(401, "Comment update failed"),
 
     USER_NOT_FOUND(404, "User not found"),
     ;

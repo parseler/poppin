482ef0af08765bcf90f1d42ac7253320bbdb9bfa
parseler <gruns0989@naver.com>
2024-07-30 12:52:12 +0900

    [feat] Implement on-site reservation and reservation inquiry functions JIRA - S11P12A806-232

--- backend/src/main/java/com/apink/poppin/api/reservation/service/OnsiteReservationService.java
+++ backend/src/main/java/com/apink/poppin/api/reservation/service/OnsiteReservationService.java
@@ -3,6 +3,7 @@ package com.apink.poppin.api.reservation.service;
 import com.apink.poppin.api.reservation.dto.OnsiteReservationDto;
 
 public interface OnsiteReservationService {
-    void saveToRedis(OnsiteReservationDto onsiteReservationDto);
-    OnsiteReservationDto getFromRedis(String phoneNumber, Long popupId);
+    OnsiteReservationDto createOnsiteReservation(OnsiteReservationDto onsiteReservationDto);
+    OnsiteReservationDto getOnsiteReservationByKakaoLink(long onsiteReservationId, long popupId);
+    OnsiteReservationDto getOnsiteReservationByPhoneNumber(String phoneNumber);
 }
--- backend/src/main/java/com/apink/poppin/api/reservation/service/OnsiteReservationServiceImpl.java
+++ backend/src/main/java/com/apink/poppin/api/reservation/service/OnsiteReservationServiceImpl.java
@@ -1,42 +1,123 @@
 package com.apink.poppin.api.reservation.service;
 
+import com.apink.poppin.api.popup.entity.Popup;
+import com.apink.poppin.api.popup.repository.PopupRepository;
 import com.apink.poppin.api.reservation.dto.OnsiteReservationDto;
+import com.apink.poppin.api.reservation.entity.OnsiteReservation;
+import com.apink.poppin.api.reservation.entity.ReservationStatement;
 import com.apink.poppin.api.reservation.repository.OnsiteReservationRepository;
-import org.springframework.data.redis.core.RedisTemplate;
+import com.apink.poppin.common.exception.dto.BusinessLogicException;
+import com.apink.poppin.common.exception.dto.ExceptionCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Map;
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.Set;
 
 @Service
+@RequiredArgsConstructor
 public class OnsiteReservationServiceImpl implements OnsiteReservationService {
 
-    private static final String RESERVATION_KEY = "onsite_reservation:";
+    private static final String RESERVATION_KEY_POPUP = "ONSITE BY POPUP_";
+    private static final String RESERVATION_KEY_PHONE = "ONSITE BY PHONE_";
 
-    private final OnsiteReservationRepository repo;
-    private final RedisTemplate<String, OnsiteReservationDto> redisTemplate;
+    // popup id를 통해 접근. 대기 번호 설정을 위해 sorted set 사용
+    private final ZSetOperations<String, OnsiteReservationDto> zSetOperations;
 
-    public OnsiteReservationServiceImpl(OnsiteReservationRepository repo, RedisTemplate<String, OnsiteReservationDto> redisTemplate) {
-        this.repo = repo;
-        this.redisTemplate = redisTemplate;
+    // 사용자가 다른 팝업에 대기하고자 하는 경우 전화번호만으로 대기 중인 정보 조회
+    private final ValueOperations<String, OnsiteReservationDto> valueOperations;
+
+    private final OnsiteReservationRepository onsiteReservationRepository;
+    private final PopupRepository popupRepository;
+
+    @Override
+    @Transactional
+    public OnsiteReservationDto createOnsiteReservation(OnsiteReservationDto onsiteReservationDto) {
+
+        String keyByPopup = RESERVATION_KEY_POPUP + onsiteReservationDto.getPopupId();
+        String keyByPhone = RESERVATION_KEY_PHONE + onsiteReservationDto.getPhoneNumber();
+
+        if (valueOperations.get(keyByPhone) != null) {
+            throw new IllegalStateException("Onsite reservation already exists");
+        }
+
+        Popup popup = popupRepository.findById(onsiteReservationDto.getPopupId())
+                .orElseThrow(() -> new BusinessLogicException(ExceptionCode.POPUP_NOT_FOUND));
+        ReservationStatement reservationStatement = ReservationStatement.builder()
+                .reservationStatementId(1L).build();
+
+        OnsiteReservation reservation = OnsiteReservation.builder()
+                .popup(popup)
+                .phoneNumber(onsiteReservationDto.getPhoneNumber())
+                .visitedDate(LocalDate.now())
+                .reservationStatement(reservationStatement)
+                .reservationCount(onsiteReservationDto.getReservationCount())
+                .build();
+
+        OnsiteReservation onsiteReservation = onsiteReservationRepository.save(reservation);
+
+        Integer waitNumber = getWaitNumber(keyByPopup);
+        onsiteReservationDto.createReservation(onsiteReservation.getOnsiteReservationId(), waitNumber);
+
+        double score = makeScore(onsiteReservationDto);
+        zSetOperations.add(keyByPopup, onsiteReservationDto, score);
+        valueOperations.set(keyByPhone, onsiteReservationDto);
+
+        return onsiteReservationDto;
     }
 
     @Override
-    public void saveToRedis(OnsiteReservationDto onsiteReservationDto) {
-        String key = RESERVATION_KEY + onsiteReservationDto.getPopupId();
-        Integer waitNumber = getWaitNumber(key);
-        onsiteReservationDto.setWaitNumber(waitNumber);
+    public OnsiteReservationDto getOnsiteReservationByKakaoLink(long onsiteReservationId, long popupId) {
 
-        redisTemplate.opsForHash().put(key, onsiteReservationDto.getPhoneNumber(), onsiteReservationDto);
+        String keyByPopup = RESERVATION_KEY_POPUP + popupId;
+        Set<Object> reservations = Collections.singleton(zSetOperations.rangeByScore(keyByPopup, 0, Double.MAX_VALUE));
+
+        for (Object obj : reservations) {
+            OnsiteReservationDto dto = (OnsiteReservationDto) obj;
+            if (dto.getOnsiteReservationId() == onsiteReservationId) {
+                return dto;
+            }
+        }
+
+        OnsiteReservation onsiteReservation = onsiteReservationRepository.findById(onsiteReservationId)
+                .orElseThrow(() -> new BusinessLogicException(ExceptionCode.ONSITE_NOT_FOUND));
+
+        return OnsiteReservationDto.builder()
+                .onsiteReservationId(onsiteReservation.getOnsiteReservationId())
+                .popupId(onsiteReservation.getPopup().getPopupId())
+                .reservationStatementId(onsiteReservation.getReservationStatement().getReservationStatementId())
+                .reservationCount(onsiteReservation.getReservationCount())
+                .phoneNumber(onsiteReservation.getPhoneNumber())
+                .visitedDate(onsiteReservation.getVisitedDate())
+                .waitNumber(onsiteReservation.getWaitNumber())
+                .build();
     }
 
     @Override
-    public OnsiteReservationDto getFromRedis(String phoneNumber, Long popupId) {
-        String key = RESERVATION_KEY + popupId;
-        return (OnsiteReservationDto) redisTemplate.opsForHash().get(key, phoneNumber);
+    public OnsiteReservationDto getOnsiteReservationByPhoneNumber(String phoneNumber) {
+
+        String key = RESERVATION_KEY_PHONE + phoneNumber;
+        OnsiteReservationDto onsiteReservationDto = valueOperations.get(key);
+
+        if (onsiteReservationDto == null) {
+            throw new BusinessLogicException(ExceptionCode.ONSITE_NOT_FOUND);
+        }
+
+        return onsiteReservationDto;
     }
 
     private Integer getWaitNumber(String key) {
-        Map<Object, Object> map = redisTemplate.opsForHash().entries(key);
-        return map.size() + 1;
+        Long size = zSetOperations.size(key);
+        if (size == null) return 1;
+
+        return size.intValue();
+    }
+
+    private double makeScore(OnsiteReservationDto onsiteReservationDto) {
+        return onsiteReservationDto.getReservationStatementId() * 10000 + onsiteReservationDto.getWaitNumber();
     }
 }

dc16c438020e539a7a24a53d9ca97a4c4ef98e39
Eingerjar <cksrb1526@naver.com>
2024-07-31 08:40:56 +0900

    [fix] fix DTOs about requests and response

--- backend/src/main/java/com/apink/poppin/api/manager/controller/ManagerController.java
+++ backend/src/main/java/com/apink/poppin/api/manager/controller/ManagerController.java
@@ -1,11 +1,15 @@
 package com.apink.poppin.api.manager.controller;
 
+import com.apink.poppin.api.manager.dto.*;
+import com.apink.poppin.api.manager.entity.Manager;
 import com.apink.poppin.api.manager.service.ManagerService;
-import com.apink.poppin.api.popup.dto.PopupDTO;
 import com.apink.poppin.api.popup.service.PopupService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,15 +22,27 @@ public class ManagerController {
     //TODO: Admin 권한 확인 후 매니저 전체 조회
     @GetMapping("/")
     ResponseEntity<?> getManagers() {
-        List<ManagerListDTO> managerList = managerService.getManagerList();
-        return ResponseEntity.ok(managerList);
+        List<Manager> managerList = managerService.getManagerList();
+        List<ManagerListDTO> result = new ArrayList<>();
+        for (Manager manager : managerList) {
+            result.add(ManagerListDTO.builder()
+                    .managerTsid(manager.getManagerTsid())
+                    .nickname(manager.getNickname())
+                    .img(manager.getImg())
+                    .build());
+        }
+        return ResponseEntity.ok(result);
     }
 
     //TODO: JWT 토큰을 통한 조회 @PathVarialbe => @ManagerTsid
     @GetMapping("/me")
     ResponseEntity<?> getMyInfo(@PathVariable Long managerTsid) {
-        ManagerResponseDTO manager = managerService.getManager(managerTsid);
-        return ResponseEntity.ok(manager);
+        Manager reponse = managerService.getManager(managerTsid);
+        return ResponseEntity.ok(ReadManagerResponseDTO.builder()
+                .managerTsid(reponse.getManagerTsid())
+                .nickname(reponse.getNickname())
+                .img(reponse.getImg())
+                .build());
     }
 
     //TODO: 위와 같은 JWT 토큰을 통한 조회 @PathVarialbe => @ManagerTsid
@@ -45,15 +61,26 @@ public class ManagerController {
 
     @GetMapping("/{managerTsid}")
     ResponseEntity<?> getManager(@PathVariable Long managerTsid) {
-        ManagerResponseDTO manager =  managerService.getManager(managerTsid);
-        return ResponseEntity.ok(manager);
+        Manager manager =  managerService.getManager(managerTsid);
+        return ResponseEntity.ok(ReadManagerResponseDTO.builder()
+                .managerTsid(manager.getManagerTsid())
+                .nickname(manager.getNickname())
+                .img(manager.getImg())
+                .build());
     }
 
     @PutMapping("/{managerTsid}")
-    ResponseEntity<?> updateManager(@PathVariable Long managerTsid, @RequestBody ManagerDTO managerDTO) {
-        managerDTO.setManagerTsid(managerTsid);
-        ManagerResponseDTO manager = managerService.updateManager(managerDTO);
-        return ResponseEntity.ok(manager);
+    ResponseEntity<?> updateManager(@PathVariable Long managerTsid, @RequestBody UpdateManagerRequestDTO request) {
+        managerService.updateManager(managerTsid, ManagerDTO.builder()
+                .nickname(request.getNickname())
+                .code(request.getCode())
+                .img(request.getImg())
+                .build());
+        Manager manager = managerService.getManager(managerTsid);
+        UpdateManagerResponseDTO response = UpdateManagerResponseDTO.builder()
+                .managerTsid(manager.getManagerTsid())
+                .build();
+        return ResponseEntity.ok(response);
     }
 
     //TODO: Admin 권한 확인 후 매니저 삭제

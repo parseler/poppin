a760d27fc286d187a0b1418ed95008ad1c602484
김찬규 <cksrb1526@naver.com>
2024-07-30 17:59:49 +0900

    [feat] Add ManagerDTO

---
+++ backend/src/main/java/com/apink/poppin/api/manager/dto/ManagerDTO.java
@@ -0,0 +1,15 @@
+package com.apink.poppin.api.manager.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class ManagerDTO {
+    private Long managerTsid;
+    private String nickname;
+    private String code;
+    private String img;
+}

e81a1c485146555363864a46a87eeb413a499c5c
parseler <gruns0989@naver.com>
2024-08-01 16:20:49 +0900

    [feat] Add scheduling and calculateRank method to OnsiteReservationService.java JIRA - S11P12A806-259

--- backend/src/main/java/com/apink/poppin/api/reservation/service/OnsiteReservationService.java
+++ backend/src/main/java/com/apink/poppin/api/reservation/service/OnsiteReservationService.java
@@ -1,12 +1,13 @@
 package com.apink.poppin.api.reservation.service;
 
 import com.apink.poppin.api.reservation.dto.OnsiteReservationDto;
+import com.apink.poppin.api.reservation.dto.OnsiteReservationRedisDto;
 import com.apink.poppin.api.reservation.dto.OnsiteReservationRequestDto;
 
 import java.util.List;
 
 public interface OnsiteReservationService {
-    OnsiteReservationDto createOnsiteReservation(OnsiteReservationDto onsiteReservationDto);
+    OnsiteReservationRedisDto createOnsiteReservation(OnsiteReservationDto onsiteReservationDto);
 
     OnsiteReservationDto getOnsiteReservationByKakaoLink(long onsiteReservationId, long popupId);
 
@@ -15,4 +16,8 @@ public interface OnsiteReservationService {
     OnsiteReservationDto changeOnsiteReservation(OnsiteReservationRequestDto onsiteReservationRequestDto);
 
     List<OnsiteReservationDto> getOnsiteReservations(long popupId);
+
+    void saveScheduling();
+
+    OnsiteReservationDto calculateRank(OnsiteReservationRedisDto redisDto);
 }

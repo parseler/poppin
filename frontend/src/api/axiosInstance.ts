c5de905156dbf88428695c4081be16abef0620b8
Eunicekk <cho_lyn@naver.com>
2024-08-02 13:27:37 +0900

    [feat] Axios user info S11P12A806-278

--- frontend/src/api/axiosInstance.ts
+++ frontend/src/api/axiosInstance.ts
@@ -4,10 +4,11 @@ import { getCookie } from '@utils/get-user-cookie';
 // const DEV_URL = "";
 // const TEST_URL = "";
 const LOCAL_URL = "http://70.12.247.215:8080";
+// const LOCAL_URL = "http://localhost:8080";
 
 const axiosInstance = axios.create({
   baseURL: LOCAL_URL, 
-  timeout: 1000,
+  timeout: 3000,
   withCredentials: true, // CORS 요청에 자격 증명을 포함
 })
 

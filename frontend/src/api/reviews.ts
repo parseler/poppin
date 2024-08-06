7a8012f82b4dad8c43da74b5946695640107d138
Eunicekk <cho_lyn@naver.com>
2024-08-06 16:31:05 +0900

    [feat] Axios review update S11P12A806-286

--- frontend/src/api/reviews.ts
+++ frontend/src/api/reviews.ts
@@ -1,4 +1,4 @@
-import { ReviewProps } from "@interface/reviews";
+import { ReviewProps, ReviewUpdateProps } from "@interface/reviews";
 import axiosInstance from "./axiosInstance";
 
 // 후기 목록 조회
@@ -15,10 +15,14 @@ export const getReviewData = async (reviewId: number) => {
 
 // 후기 생성
 export const createReviewData = async (popupId: number, review: ReviewProps) => {
-  const response = await axiosInstance.post(`/api/${popupId}/reviews`, review);
+  const response = await axiosInstance.post(`/api/popups/${popupId}/reviews`, review);
+  return response.data;
+}
+
+// 후기 수정
+export const updateReviewData = async (reviewId: number, review: ReviewUpdateProps) => {
+  const response = await axiosInstance.put(`/api/reviews/${reviewId}`, review);
   return response.data;
 }
 
 // 후기 삭제
-
-// 후기 수정
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -5,15 +5,11 @@ import naver from "@assets/login/icon_naver.png";
 
 const Login = () => {
   const handleKakaoLogin = () => {
-    const KAKAO_KEY = import.meta.env.VITE_KAKAO_API_KEY;
-    const KAKAO_URI = import.meta.env.VITE_KAKAO_REDIRECT_URI;
-    window.location.href = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${KAKAO_KEY}&redirect_uri=${KAKAO_URI}`;
-  };
+    window.location.href = `http://localhost/oauth2/authorization/kakao`;
+  }
 
   const handleNaverLogin = () => {
-    const NAVER_KEY = import.meta.env.VITE_NAVER_API_KEY;
-    const NAVER_URI = import.meta.env.VITE_NAVER_REDIRECT_URI;
-    window.location.href = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_KEY}&redirect_uri=${NAVER_URI}`;
+    window.location.href = `http://localhost/oauth2/authorization/naver`;
   };
 
   return (

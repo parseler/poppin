b06cbfc27a1cc2d49159203346976ffd699f6b3d
dahwan <sinphoto@naver.com>
2024-08-07 11:00:49 +0900

    [fix] Fix authentication, logout, login logic and Update refreshToken, accessToken expired time

--- backend/src/main/java/com/apink/poppin/common/auth/filter/CustomLogoutFilter.java
+++ backend/src/main/java/com/apink/poppin/common/auth/filter/CustomLogoutFilter.java
@@ -1,8 +1,9 @@
 package com.apink.poppin.common.auth.filter;
 
 import com.apink.poppin.common.auth.service.AuthService;
+import com.apink.poppin.common.exception.dto.BusinessLogicException;
+import com.apink.poppin.common.exception.dto.ExceptionCode;
 import com.apink.poppin.common.util.JwtTokenUtil;
-import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
@@ -18,7 +19,7 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class CustomLogoutFilter extends GenericFilterBean {
 
-    private final JwtTokenUtil jwtUtil;
+    private final JwtTokenUtil jwtTokenUtil;
     private final AuthService authService;
 
     @Override
@@ -39,6 +40,7 @@ public class CustomLogoutFilter extends GenericFilterBean {
         String requestMethod = request.getMethod();
         if(!requestMethod.equals("POST")) {
             filterChain.doFilter(request, response);
+            return;
         }
 
         // refresh 토큰 추출
@@ -54,36 +56,20 @@ public class CustomLogoutFilter extends GenericFilterBean {
 
         // refresh 토큰 존재 확인
         if(refresh == null) {
-            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            return;
-        }
-
-        // refresh 토큰 만료 확인
-        try {
-            jwtUtil.isExpired(refresh);
-        } catch (ExpiredJwtException e) {
-            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            return;
+            throw new BusinessLogicException(ExceptionCode.REFRESH_TOKEN_ERROR);
         }
 
         // refresh 토큰인지 확인
-        String category = jwtUtil.getCategory(refresh);
+        String category = jwtTokenUtil.getCategory(refresh);
         if(!category.equals("refresh")) {
-            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            throw new BusinessLogicException(ExceptionCode.REFRESH_TOKEN_ERROR);
         }
 
-        // DB에 존재하는지 확인
-        String role = jwtUtil.getRole(refresh);
-        Boolean isExist = authService.isExist(refresh, role);
-
-        if(!isExist) {
-            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            return;
-        }
-
+        String role = jwtTokenUtil.getRole(refresh);
+        long tsid = jwtTokenUtil.getUsername(refresh);
         // 로그아웃 진행
         // Refresh 토큰 DB에서 제거
-        authService.deleteRefreshToken(refresh, role);
+        authService.deleteRefreshToken(tsid, role);
 
         // RefreshToken Cookie 값 0
         Cookie cookie = new Cookie("refresh", null);

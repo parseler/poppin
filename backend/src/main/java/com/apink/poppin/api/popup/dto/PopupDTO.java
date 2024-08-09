9c88c2204bdf0664d4a530c74df3e277e1d3faad
parseler <gruns0989@naver.com>
2024-08-09 14:37:27 +0900

    [refactor] Modify import packages JIRA - S11P12A806-154

--- backend/src/main/java/com/apink/poppin/api/popup/dto/PopupDTO.java
+++ backend/src/main/java/com/apink/poppin/api/popup/dto/PopupDTO.java
@@ -1,14 +1,11 @@
 package com.apink.poppin.api.popup.dto;
 
-import com.apink.poppin.api.popup.entity.PopupImage;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
-import java.util.Date;
 import java.util.List;
 
 @Builder
--- backend/src/main/java/com/apink/poppin/api/popup/dto/PopupRequestDTO.java
+++ backend/src/main/java/com/apink/poppin/api/popup/dto/PopupRequestDTO.java
@@ -1,21 +1,13 @@
 package com.apink.poppin.api.popup.dto;
 
-import com.apink.poppin.api.manager.entity.Manager;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import org.hibernate.annotations.ColumnDefault;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDate;

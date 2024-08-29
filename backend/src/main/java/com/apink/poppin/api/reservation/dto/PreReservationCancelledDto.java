a0daf6ba2c577e8bb6dd11cfa8124f0ad27e919a
dahwan <sinphoto@naver.com>
2024-08-29 14:23:16 +0900

    [fix] repair project

---
+++ backend/src/main/java/com/apink/poppin/api/reservation/dto/PreReservationCancelledDto.java
@@ -0,0 +1,36 @@
+package com.apink.poppin.api.reservation.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Date;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PreReservationCancelledDto {
+    private Long preReservationId;
+    private String userTsid;
+    private Long popupId;
+    private String images;
+
+    @JsonSerialize(using = LocalDateSerializer.class)
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private LocalDate reservationDate;
+    private LocalTime reservationTime;
+    private int reservationCount;
+
+    @JsonSerialize(using = LocalDateSerializer.class)
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private LocalDate createdAt;
+    private Long reservationStatementId;
+}
